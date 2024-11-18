e900b32a63532128028ae0255b70bd45e38a2175
Santi Garnier <108945065+santiagogarnier@users.noreply.github.com>
2024-11-18 17:26:46 -0300

    Primeras estructuras

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct Cancion
+{
+    string nombre;
+    string artista;
+    Cancion *sgte;
+};
+
+// Prototipos de funciones
+Cancion dameCancion();
+Cancion *crearNodo(Cancion *&pila, Cancion tema);
+
+int main()
+{
+
+    return 0;
+}
+
+Cancion dameCancion()
+{
+    Cancion tema;
+    cout << "Ingrese un tema que desea escuchar:";
+    cin >> tema.nombre;
+    cout << "Artista de la cancion: ";
+    cin >>tema.artista;
+    return tema;
+}
+
+Cancion* crearNodo(Cancion tema)
+{
+    Cancion *nodoCancion = new Cancion;
+    nodoCancion->nombre = tema.nombre;
+    nodoCancion->sgte = nullptr;
+    return nodoCancion;
+}
+
+
+//Agrega canciones a la pila de reproduccion, quedando primera.
+void pushCancion(Cancion*& pila, Cancion tema)
+{
+    Cancion* nodoCancion = crearNodo(tema);
+    nodoCancion->sgte = nullptr;
+    pila = nodoCancion;
+}
+
+
+void popCancion(Cancion*& pila){
+    if (pila == nullptr)
+    {
+        cout << "La playlist esta vacia."<<endl;
+        Cancion cancionNula;
+        cancionNula.nombre = "_";
+        return cancionNula;
+    }
+    Cancion cancionActual = *pila;
+    Cancion* temp = pila;
+    pila = pila->sgte;
+    delete temp;
+    return cancionActual;
+    
+}
