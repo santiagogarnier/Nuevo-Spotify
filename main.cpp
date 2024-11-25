15291c79a089504aa5a02848ba17889945fbed66
Santi Garnier <108945065+santiagogarnier@users.noreply.github.com>
2024-11-25 20:35:02 -0300

    ejercicio completo

--- main.cpp
+++ main.cpp
@@ -18,16 +18,82 @@ struct Registro
 };
 
 // Prototipos de funciones
+void mostrarMenu();
 Cancion dameCancion();
 Cancion *crearNodo(Cancion *&pila, Cancion tema);
 void pushCancion(Cancion *&pila, Cancion tema);
-Cancion popCancion(Cancion *&pila, Cancion tema);
+Cancion popCancion(Cancion *&pila);
 void revertirPlaylist(Cancion *&pila);
+Registro *crearNodoRegistro(Cancion cancionGuardar, short estrellas);
+Registro *calificar_listar(Cancion cancionAgregar, Registro *historial);
+void imprimirListaSimple(Registro *inicio);
 
 int main()
 {
+    int menu = 0;
+    Cancion *playlist = nullptr;
+    Registro *historial = nullptr;
+    Cancion nuevaCancion;
+    Cancion cancionActual;
+    cancionActual.nombre = "Primer recorrido";
+    cout << "Bienvenido a Spotify " << endl;
+    while (menu != 6)
+    {
+        mostrarMenu();
+        cin >> menu;
+        switch (menu)
+        {
+        case 1:
+            pushCancion(playlist, nuevaCancion);
+            nuevaCancion = dameCancion();
+            break;
+
+        case 2:
+            if (cancionActual.nombre != "Primer recorrido")
+            {
+                historial = calificar_listar(cancionActual, historial);
+            }
+            cancionActual = popCancion(playlist);
+            if (cancionActual.nombre != "_")
+            {
+                cout << "Ahora se reproduce: " << cancionActual.nombre << " > " << endl;
+            }
+            break;
+        case 3:
+            if (playlist != NULL)
+            {
+                cout << "La siguiente cancion a reproducir es: " << playlist->sgte->nombre << ">" << endl;
+            }
+            else
+            {
+                cout << "No hay mas canciones en la playlist." << endl;
+            }
+            break;
+        case 4:
+            revertirPlaylist(playlist);
+            break;
+        case 5:
+            imprimirListaSimple(historial);
+            break;
+        case 6:
+            cout << "Saliendo de Spotify..." << endl;
+            break;
+        default:
+            break;
+        }
+    }
 
     return 0;
+}
+
+void mostrarMenu()
+{
+    cout << "1. Agregar nueva cancion a la playlist." << endl;
+    cout << "2. Reproducir siguiente cancion." << endl;
+    cout << "3. Siguiente cancion en la playlist." << endl;
+    cout << "4.Revertir la playlist." << endl;
+    cout << "5. Mostrar historial." << endl;
+    cout << "6. Salir." << endl;
 }
 
 Cancion dameCancion()
@@ -80,19 +146,51 @@ void revertirPlaylist(Cancion *&pila)
     while (pila != nullptr)
     {
         auxiliar = popCancion(pila);
-        pushCancion(pilaaux,auxiliar);
-
+        pushCancion(pilaaux, auxiliar);
     }
     pila = pilaaux;
     cout << "Se revirtio con exito la playlist.";
-
 }
 
 Registro *crearNodoRegistro(Cancion cancionGuardar, short estrellas)
 {
-   Registro* nodoRegistro = new Registro;
-   nodoRegistro->estrellas = estrellas;
-   nodoRegistro->song = cancionGuardar;
-   nodoRegistro->siguiente = nullptr;
-   return nodoRegistro;
+    Registro *nodoRegistro = new Registro;
+    nodoRegistro->estrellas = estrellas;
+    nodoRegistro->song = cancionGuardar;
+    nodoRegistro->siguiente = nullptr;
+    return nodoRegistro;
+}
+
+Registro *calificar_listar(Cancion cancionAgregar, Registro *historial)
+{
+    short estrellas;
+    cout << "Puntue de 1 a 5 estrellas la ultima cancion: ";
+    cin >> estrellas;
+    cin.ignore();
+
+    Registro *nuevoNodoRegistro = crearNodoRegistro(cancionAgregar, estrellas);
+    if (historial == nullptr || cancionAgregar.artista < historial->song.artista)
+    {
+        nuevoNodoRegistro->siguiente = historial;
+        historial = nuevoNodoRegistro;
+        return historial;
+    }
+    Registro *actual = historial;
+    while (actual->siguiente != nullptr && actual->siguiente->song.artista < cancionAgregar.artista)
+    {
+        actual = actual->siguiente;
+    }
+    nuevoNodoRegistro->siguiente = actual->siguiente;
+    actual->siguiente = nuevoNodoRegistro;
+    return historial;
+}
+
+void imprimirListaSimple(Registro *inicio)
+{
+    Registro *actual = inicio;
+    while (actual != nullptr)
+    {
+        cout << "Titulo: " << actual->song.nombre << ", Artista: " << actual->song.artista << " ,estrellas: " << actual->estrellas << endl;
+        actual = actual->siguiente;
+    }
 }
